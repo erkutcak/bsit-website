25573c9d4391a02917bfed1dc3a26deea7daaebe
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-08-07 19:42:50 +0300

    auth fixes

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,18 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 const Blog = () => {
+
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session == null) router.push("/")
+  }, [session])
+
   return (
     <div>Blog</div>
   )
--- app/myprofile/page.js
+++ app/myprofile/page.js
@@ -1,6 +1,19 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 const MyProfile = () => {
+
+  const { data: session } = useSession()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session == null) router.push("/")
+  }, [session])
+
+
   return (
     <div>My Profile</div>
   )
